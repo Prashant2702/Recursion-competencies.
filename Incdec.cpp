0041a120c5c95ea4a07b68c1fe50025431d4dfbf
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2023-02-02 13:06:19 +0530

    Printing in Increasing or decreasing order.

---
+++ Incdec.cpp
@@ -0,0 +1,29 @@
+//Printing the numbers in Increasing or decreasing order.
+#include<iostream>
+using namespace std;
+
+void dec(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<endl;
+    dec(n-1);
+}
+
+void inc(int n){
+     if(n==0){
+        return;
+    }
+
+    int(n-1);
+    cout<<n<<endl;
+}
+
+int main(){
+int n;
+cin>>n;
+
+dec(n);
+inc(n);
+    return 0;
+}
